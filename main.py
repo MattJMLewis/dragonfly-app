8be8e11d735dfa1a41b2754201df22f84adb136e
MattJMLewis <41051576+MattJMLewis@users.noreply.github.com>
2020-02-28 21:33:09 +0000

    Updated with new builder.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from routes import routes
-from dragonfly import request
 from wsgiref.simple_server import make_server
+
+from dragonfly import request
+
+from routes import routes
 
 
 def app(environ, start_response):
-
     request.update_environ(environ)
     response = routes.dispatch_route()
 
